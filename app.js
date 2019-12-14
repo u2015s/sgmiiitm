185cc52ac5c51d3d1857fe2c3b5c9bb6a7e872dd
u2015s <utkarhsingh2015@gmail.com>
2019-12-14 22:30:25 +0530

    updated

--- app.js
+++ app.js
@@ -126,12 +126,6 @@ app.post("/info",function(req, res){
         })
     });
 
-    var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-    var server_host = process.env.YOUR_HOST || '0.0.0.0';
-    server.listen(server_port, server_host, function() {
-        console.log('Listening on port %d', server_port);
-    })
-
-// app.listen(process.env.port || 3000);
-
-// console.log('Running at Port 3000');
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server started");
+})
