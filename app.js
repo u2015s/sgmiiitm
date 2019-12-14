9647a90d344d3a18b423af00b50a7fb24769f1ed
u2015s <utkarhsingh2015@gmail.com>
2019-12-14 22:26:18 +0530

    app.js updadaf

--- app.js
+++ app.js
@@ -126,7 +126,11 @@ app.post("/info",function(req, res){
         })
     });
 
-
+    var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+    var server_host = process.env.YOUR_HOST || '0.0.0.0';
+    server.listen(server_port, server_host, function() {
+        console.log('Listening on port %d', server_port);
+    })
 
 // app.listen(process.env.port || 3000);
 
